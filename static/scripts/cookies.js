a779f13ebe84511edee149af090f0e0ee9ae6c59
francescomerighi1202 <francescomerighi61@gmail.com>
2024-05-06 10:57:09 +0200

    Aggiornamento sitemap e menù - Bug fix

--- static/scripts/cookies.js
+++ static/scripts/cookies.js
@@ -5,51 +5,6 @@ state.style.fontSize = "clamp(0.75rem, 3vw, 1rem)";
 
 state.textContent = document.cookie;
 
-// Scroll animations
-let sectionsFromRight = document.querySelectorAll('.animated-right');
-let sectionsFromUp = document.querySelectorAll('.animated-up');
-let sectionsFromLeft = document.querySelectorAll('.animated-left');
-let sectionsFade = document.querySelectorAll('.animated-fade');
-
-window.onscroll = () => {
-  sectionsFade.forEach(sec => {
-    let top = window.scrollY + 500;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add('fade-in');
-    }
-  })
-  sectionsFromLeft.forEach(sec => {
-    let top = window.scrollY + 500;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add('fade-left');
-    }
-  })
-  sectionsFromUp.forEach(sec => {
-    let top = window.scrollY + 500;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add('fade-up');
-    }
-  })
-  sectionsFromRight.forEach(sec => {
-    let top = window.scrollY + 500;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add('fade-right');
-    }
-  })
-}
-
 // Hamburger menu
 const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav__list');
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,26 +1,3 @@
-// Slideshow
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    const slides = document.getElementsByClassName('image__container');
-
-    // Nascondi tutte le immagini
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-
-    // Mostra l'immagine corrente
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = 'block';
-
-    // Ripeti la funzione dopo 2 secondi (2000ms)
-    setTimeout(showSlides, 3000);
-}
-
 // Hamburger menu
 const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav__list');
@@ -39,77 +16,14 @@ document.querySelectorAll('.nav__link, .nav__language').forEach(n =>
         languages.classList.remove('active');
 }))
 
-// Manual slideshow
-let photos = ["/static/images/indoor1.jpg", "/static/images/indoor2.jpg", "/static/images/indoor3.jpg", "/static/images/indoor4.jpg",
-    "/static/images/outdoor2.jpg", "/static/images/bar2.jpg"];
-
-let manualImg = document.querySelector('.slideshow__manual img')
-let count = 0;
-
-function next() {
-    count++;
-
-    if (count >= photos.length) {
-      count = 0;
-      manualImg.src = photos[count];
-    } else {
-      manualImg.src = photos[count];
-    }
-}
-
-// Scroll animations
-let sectionsFromRight = document.querySelectorAll('.animated-right');
-let sectionsFromUp = document.querySelectorAll('.animated-up');
-let sectionsFromLeft = document.querySelectorAll('.animated-left');
-let sectionsFade = document.querySelectorAll('.animated-fade');
-
-window.onscroll = () => {
-  sectionsFade.forEach(sec => {
-    let top = window.scrollY + 500;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add('fade-in');
-    }
-  })
-  sectionsFromLeft.forEach(sec => {
-    let top = window.scrollY + 500;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add('fade-left');
-    }
-  })
-  sectionsFromUp.forEach(sec => {
-    let top = window.scrollY + 500;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add('fade-up');
-    }
-  })
-  sectionsFromRight.forEach(sec => {
-    let top = window.scrollY + 500;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add('fade-right');
-    }
-  })
-}
-
 // Cookie banner
 let banner = document.querySelector('.cookie-banner');
 let accept = document.querySelector('#accept');
 let refuse = document.querySelector('#refuse');
 let pref = document.querySelector('#pref');
 
-let map = document.querySelector('.location iframe');
-let errorMap = document.querySelector('.location .nocookie__text');
+let map = document.querySelector('.where-when iframe');
+let errorMap = document.querySelector('.nocookie-text');
 
 setCookie = (cName, cValue, expDays) => {
   let date = new Date();
